f29fae86b18c8e1c669f041c04faa6e8374bb008
dz_paji <zhangqian2002@outlook.com>
2023-03-07 22:47:31 +0000

    Display impression info

--- app/src/main/java/org/comp2211/controllers/HomepageController.java
+++ app/src/main/java/org/comp2211/controllers/HomepageController.java
@@ -3,18 +3,24 @@ package org.comp2211.controllers;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import org.comp2211.model.ClickCalculator;
+import org.comp2211.model.ImpressionCalculator;
 
 public class HomepageController {
 
     public ClickCalculator clickCalculator = new ClickCalculator();
     public Button refreshButton;
     public Label totalClicksLabel, totalCostLabel, cPCLabel;
+    private ImpressionCalculator impressionCalculator = new ImpressionCalculator();
+    public Label totalImprLabel, uniqueImprLabel, cPMLabel;
 
     public void handleRefreshButton(){
         System.out.println(clickCalculator.getTotalClicks());
         totalClicksLabel.setText(Integer.toString(clickCalculator.getTotalClicks()));
         totalCostLabel.setText(Double.toString(clickCalculator.getTotalCost()));
-        cPCLabel.setText(Double.toString(clickCalculator.getCPC()));
+        cPCLabel.setText("£" + clickCalculator.getCPC());
+        totalImprLabel.setText(String.valueOf(impressionCalculator.getImpr()));
+        uniqueImprLabel.setText(String.valueOf(impressionCalculator.getUniques()));
+        cPMLabel.setText("£" + impressionCalculator.getCPM());
     }
 
 }
