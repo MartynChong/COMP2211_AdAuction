82298bffe5518d9db55f91b9002ccaf2a917766d
dz_paji <zhangqian2002@outlook.com>
2023-03-07 16:12:12 +0000

    Import impression from csv

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.comp2211 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.opencsv;
     exports org.comp2211;
     exports org.comp2211.controllers;
 }
