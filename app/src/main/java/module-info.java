d1262cf884ec9cfa929f30f1d3b341e38d57f8de
Martyn Chong <qtc1g21@soton.ac.uk>
2023-03-07 15:51:24 +0000

    Fixed module-info error

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,10 +1,7 @@
 module org.comp2211 {
     requires javafx.controls;
-<<<<<<< HEAD
     requires javafx.fxml;
-=======
     requires java.sql;
->>>>>>> 23bd276b73460002b264d263b97dcb91f46ef2d1
     exports org.comp2211;
     exports org.comp2211.controllers;
 }
